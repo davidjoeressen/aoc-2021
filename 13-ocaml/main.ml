f6ca8468b55087420711219d6dc51aa1699e99dd
David Joeressen <david@joeressen.net>
2021-12-14 09:07:44 +0900

    refactor

--- 13-ocaml/main.ml
+++ 13-ocaml/main.ml
@@ -17,39 +17,43 @@ type instruction =
   | X of int
   | Y of int
 
-let parse_point p =
-  let x = String.split_on_char ',' p in
-    (int_of_string @@ List.nth x 0, int_of_string @@ List.nth x 1)
+let parse_point p = match (String.split_on_char ',' p) with
+  | [x;y] -> (int_of_string x, int_of_string y)
+  | _ -> (-1,-1)
 
 let parse_instruction i =
   String.split_on_char ' ' i
   |> Fun.flip List.nth 2
   |> String.split_on_char '='
-  |> (fun x -> (if (List.nth x 0) = "x" then X (int_of_string @@ List.nth x 1) else Y (int_of_string @@ List.nth x 1)))
+  |> (fun s -> match s with
+       | [x;y] -> let y' = int_of_string y in if x = "x" then [X y'] else [Y y']
+       | _ -> [])
 
 let parse_file (points,instructions) =
-  (List.map parse_point points, List.map parse_instruction instructions)
+  (List.map parse_point points, List.concat_map parse_instruction instructions)
 
 let fold_point n x =
   if x < n then x
   else if x > n then n - (x - n)
   else -1
 
+let elem x xs = List.exists (fun a -> a = x) xs
+
 let rec nub = function
   | [] -> []
-  | x :: xs -> if List.exists (fun a -> a = x) xs then nub xs else x :: nub xs
+  | x :: xs -> if elem x xs then nub xs else x :: nub xs
 
 let both_positive (x,y) = x >= 0 && y >= 0
 
-let apply instr points = match instr with
-  | X n -> nub @@ List.filter both_positive @@ List.map (fun (x,y) -> (fold_point n x, y)) points
-  | Y n -> nub @@ List.filter both_positive @@ List.map (fun (x,y) -> (x, fold_point n y)) points
+let apply instr points = nub @@ List.filter both_positive @@ match instr with
+  | X n -> List.map (fun (x,y) -> (fold_point n x, y)) points
+  | Y n -> List.map (fun (x,y) -> (x, fold_point n y)) points
 
 let format_points points =
   let (xs,ys) = List.split points in
     List.init (succ @@ List.fold_left max 0 ys) (fun y ->
       List.init (succ @@ List.fold_left max 0 xs) (fun x ->
-        if (List.exists (fun a -> a = (x,y)) points) then "#" else " "))
+        if (elem (x,y) points) then "#" else " "))
 
 let solve s =
   let (points,instructions) = read_file s |> split_lines |> parse_file in
