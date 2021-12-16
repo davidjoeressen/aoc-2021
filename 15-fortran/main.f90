138b5f2da6b3c7d8b6b7d4eda4db8bc236763b43
David Joeressen <david@joeressen.net>
2021-12-17 01:23:13 +0900

    use priority queue

--- 15-fortran/main.f90
+++ 15-fortran/main.f90
@@ -1,10 +1,15 @@
 program aoc_2021_day_15
   implicit none
   type coord
-    integer :: x, y
-  end type
+    integer :: x, y, priority
+  end type coord
+  type queue
+    type(coord), dimension(2500000) :: heap
+    integer :: last
+  end type queue
+
   integer :: nargs
-  character (256) :: filename
+  character(len=256) :: filename
   integer, allocatable, dimension(:,:) :: board
   integer, allocatable, dimension(:,:) :: bigger_board
 
@@ -54,28 +59,7 @@ contains
         end do
       end do
     end do
-
   end subroutine generate_bigger_board
-
-  pure type(coord) function smallest_positive(board)
-    implicit none
-    integer, dimension(:,:), intent(in) :: board
-    integer :: i, j
-    integer :: smallest
-
-    smallest = 1000000
-    smallest_positive%x = 1
-    smallest_positive%y = 1
-    do j=1,size(board,2)
-      do i=1,size(board,1)
-        if (board(i,j) < smallest .and. board(i,j) >= 0) then
-          smallest = board(i,j)
-          smallest_positive%x = i
-          smallest_positive%y = j
-        end if
-      end do
-    end do
-  end function smallest_positive
 
   pure function neighbours(c) result(cs)
     implicit none
@@ -91,12 +75,94 @@ contains
     cs(4)%y = c%y + 1
   end function neighbours
 
-  pure integer function dijkstra(board)
+  subroutine init(q)
+    type(queue), intent(inout) :: q
+    q%last = 0
+  end subroutine init
+
+  subroutine swap(q, i, j)
+    type(queue), intent(inout) :: q
+    integer, intent(in) :: i, j
+    type(coord) :: tmp
+    if (i >= 1 .and. i <= q%last .and. j >= 1 .and. j <= q%last) then
+      tmp = q%heap(i)
+      q%heap(i) = q%heap(j)
+      q%heap(j) = tmp
+    end if
+  end subroutine swap
+
+  subroutine sink(q, i)
+    type(queue), intent(inout) :: q
+    integer, intent(in) :: i
+    integer :: j, k
+    integer :: p1,p2
+
+    j = i
+    k = j * 2
+    do while (k <= q%last)
+      k = j * 2
+      p1 = q%heap(k)%priority
+      if (k+1 <= q%last) then
+        p2 = q%heap(k+1)%priority
+      else
+        p2 = 1000000
+      end if
+      if (q%heap(j)%priority > p1 .or. q%heap(j)%priority > p2) then
+        if (p1 < p2) then
+          call swap(q,j,k)
+          j = k
+        else
+          call swap(q,j,k+1)
+          j = k+1
+        end if
+      else
+        exit
+      end if
+    end do
+  end subroutine sink
+
+  subroutine swim(q, i)
+    type(queue), intent(inout) :: q
+    integer, intent(in) :: i
+    type(coord) :: tmp
+    integer :: j, k
+
+    j = i
+    do while (j > 1)
+      k = j / 2
+      if (q%heap(j)%priority < q%heap(k)%priority) then
+        call swap(q,j,k)
+        j = k
+      else
+        exit
+      end if
+    end do
+  end subroutine swim
+
+  function get(q) result(c)
+    type(queue), intent(inout) :: q
+    type(coord) :: c
+    c = q%heap(1)
+    q%heap(1) = q%heap(q%last)
+    q%last = q%last - 1
+    call sink(q,1)
+  end function get
+
+  subroutine insert(q, c)
+    type(queue), intent(inout) :: q
+    type(coord), intent(in) :: c
+    q%last = q%last + 1
+    q%heap(q%last) = c
+    call swim(q,q%last)
+  end subroutine insert
+
+  integer function dijkstra(board)
     implicit none
     integer, dimension(:,:), intent(in) :: board
     integer, dimension(size(board,1),size(board,2)) :: paths
     integer :: i, j, candidate
     type(coord) :: cur, check(4)
+    type(queue) :: q
 
     ! initialize
     do j=1,size(board,2)
@@ -108,18 +174,19 @@ contains
     cur%x = 1
     cur%y = 1
 
-    do while (cur%x /= size(board,2) .OR. cur%y /= size(board,1))
+    do while (cur%x /= size(board,2) .or. cur%y /= size(board,1))
       check = neighbours(cur)
       do i=1,4
-        if (check(i)%x > 0 .AND. check(i)%y > 0 .AND. check(i)%x <= size(board,1) .AND. check(i)%y <= size(board,1)) then
+        if (check(i)%x > 0 .and. check(i)%y > 0 .and. check(i)%x <= size(board,1) .and. check(i)%y <= size(board,1)) then
           candidate = paths(cur%x,cur%y) + board(check(i)%x,check(i)%y)
           if (paths(check(i)%x,check(i)%y) > candidate) then
             paths(check(i)%x,check(i)%y) = candidate
+            check(i)%priority = candidate
+            call insert(q,check(i))
           end if
         end if
       end do
-      paths(cur%x,cur%y) = -1
-      cur = smallest_positive(paths)
+      cur = get(q)
     end do
     dijkstra = paths(size(board,1),size(board,2))
   end function dijkstra
